366d59338fa526d96486e168bad8ad9a8a375499
Gunlek <fabien.aubret@gmail.com>
2019-04-20 17:46:51 +0200

    Added dark mode capabilities

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:app_vestiaires/login.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,18 +9,53 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    return MyAppStateful();
+  }
+}
+
+class MyAppStateful extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+
+}
+
+class MyAppState extends State<MyAppStateful> {
+
+  bool _darkMode;
+
+  @override
+  void initState() {
+    super.initState();
+    _isDarkMode();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+          primarySwatch: Colors.green,
+          brightness: _darkMode ? Brightness.dark : Brightness.light
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Vestiaire 2019'),
-          backgroundColor: Colors.green,
-        ),
-        body: LoginPage()
+          appBar: AppBar(
+            title: Text('Vestiaire 2019'),
+            backgroundColor: Colors.green,
+          ),
+          body: LoginPage()
       ),
     );
   }
+
+  _isDarkMode() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool darkMode = prefs.getBool("darkMode") != null ? prefs.getBool("darkMode") : false;
+    this.setState((){
+      _darkMode = darkMode;
+    });
+  }
+
 }
