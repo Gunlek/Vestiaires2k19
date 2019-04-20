19df026c0fe6de10bd08f0b11b19c69f8cb76cdb
Gunlek <fabien.aubret@gmail.com>
2019-04-20 12:22:10 +0200

    Added Dialog to remove belongings from cloakroom

---
+++ lib/components/Dialogs.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:mysql1/mysql1.dart' as mysql;
+
+class Dialogs {
+
+  information(BuildContext context, List<String> data){
+    /*
+      data[0] => belongings_id
+      data[1] => belongings_type
+      data[2] => belongings_cloakroom
+      data[3] => belongings_number
+      data[4] => belongings_location
+      data[5] => belongings_info
+      data[6] => vestiaire_name
+     */
+
+    TextEditingController DescriptionController = TextEditingController(text: data[1]);
+    TextEditingController CodeController = TextEditingController(text: data[3]);
+    TextEditingController CloakroomController = TextEditingController(text: data[6]);
+    TextEditingController LocationController = TextEditingController(text: data[4]);
+    TextEditingController InfoController = TextEditingController(text: data[5]);
+    return showBottomSheet(
+      context: context,
+      builder: (BuildContext context){
+        return Container(
+          padding: EdgeInsets.fromLTRB(20.0, 0, 20.0, 20.0),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.arrow_downward),
+                    Text("Glisser vers le bas pour disparaitre"),
+                    Icon(Icons.arrow_downward)
+                  ]
+              ),
+
+              Padding(padding: EdgeInsets.all(15.0)),
+
+              RichText(
+                text: TextSpan(
+                  text: "Emplacement: ",
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                  children: <TextSpan>[
+                    TextSpan(text: data[6] + " - " + data[4], style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold, fontSize: 20))
+                  ]
+                )
+              ),
+
+              Padding(padding: EdgeInsets.all(8.0)),
+
+              Text("Code: "),
+              TextField(
+                controller: CodeController,
+                enabled: false,
+              ),
+
+              Padding(padding: EdgeInsets.all(8.0)),
+
+              Text("Description: "),
+              TextField(
+                controller: DescriptionController,
+                enabled: false,
+              ),
+
+              Padding(padding: EdgeInsets.all(8.0)),
+
+              Text("Vestiaire: "),
+              TextField(
+                controller: CloakroomController,
+                enabled: false,
+              ),
+
+              Padding(padding: EdgeInsets.all(8.0)),
+
+              Text("Emplacement: "),
+              TextField(
+                controller: LocationController,
+                enabled: false,
+              ),
+
+              Padding(padding: EdgeInsets.all(8.0)),
+
+              Text("Informations: "),
+              TextField(
+                controller: InfoController,
+                enabled: false,
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  RaisedButton(
+                    color: Colors.red,
+                    child: Center(child: Text('Masquer', style: TextStyle(color: Colors.white))),
+                    onPressed: (){
+                      Navigator.pop(context);
+                    },
+                  ),
+                  RaisedButton(
+                    color: Colors.blue,
+                    child: Center(child: Text('Récupérer', style: TextStyle(color: Colors.white))),
+                    onPressed: (){
+                      _removeBelongingsFromDatabase(data, context);
+                    },
+                  )
+                ]
+              )
+            ]
+          )
+        );
+      }
+    );
+  }
+
+  _removeBelongingsFromDatabase(List<String> data, BuildContext context) async {
+    Scaffold.of(context).showSnackBar(SnackBar(content: Text("Récupération en cours...")));
+    int belongingsId = int.tryParse(data[0]);
+    var sqlSettings = mysql.ConnectionSettings(
+        host: 'ftp.simple-duino.com',
+        port: 3306,
+        user: 'vestiaires_2k19',
+        password: 'emL3xC7jKCx7Nb5n',
+        db: 'vestiaires_2k19'
+    );
+    var conn = await mysql.MySqlConnection.connect(sqlSettings);
+    conn.query("DELETE FROM belongings WHERE belongings_id = ?", [belongingsId]);
+    Scaffold.of(context).hideCurrentSnackBar();
+    Scaffold.of(context).showSnackBar(SnackBar(content: Text("Affaire récupérée..."), backgroundColor: Colors.green));
+    Navigator.pop(context);
+  }
+
+}
